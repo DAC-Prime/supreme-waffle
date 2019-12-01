cd2991a6ca86b9d09174d404eb7d95220f30dfb2
ziyaoh <ziyaoh@uw.edu>
2019-12-01 11:47:41 -0500

    dac a2c agent

--- dac/dac_a2c.py
+++ dac/dac_a2c.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def cat(x, size):
     x = x[:size]
-    return torch.cat(x).detach()
+    return torch.cat(x)
 
 
 class MdpType(Enum):
@@ -25,23 +25,19 @@ class DACA2CAgent:
         self.entropy_weight = 0.01
 
         # config
-        self.num_workers = 1
+        self.num_workers = 4
         self.discount = 0.99
         self.use_gae = True
         self.gae_tau = 0.95
         self.optimization_epochs = 5
-        # self.optimization_epochs = 50
         self.ppo_clip_param = 0.2
 
         self.cur_steps = 0
         self.max_steps = steps
-        self.num_steps = 2048
-        # self.num_steps = 512
+        self.num_steps = 5
         self.env_name = env_name
-        self.num_envs = 1
+        self.num_envs = self.num_workers
         self.num_options = 4
-        self.mini_batch_size = 64
-        # self.mini_batch_size = 32
         
         self.is_init_states = tensor(np.ones((self.num_workers))).byte()
         self.worker_index = tensor(np.arange(self.num_workers)).long()
@@ -67,11 +63,11 @@ class DACA2CAgent:
         # curtime = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
         curtime = datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_{:04d}".format(random.randint(1,9999))
         self.train_logger = logging.getLogger("train")
-        self.eval_logger = logging.getLogger("eval")
+        # self.eval_logger = logging.getLogger("eval")
         self.train_logger.setLevel(logging.INFO)
-        self.eval_logger.setLevel(logging.INFO)
-        self.train_logger.addHandler(logging.FileHandler("{path}/dac_ppo_train_{curtime}.log".format(path=path, curtime=curtime), "w"))
-        self.eval_logger.addHandler(logging.FileHandler("{path}/dac_ppo_eval_{curtime}.log".format(path=path, curtime=curtime), "w"))
+        # self.eval_logger.setLevel(logging.INFO)
+        self.train_logger.addHandler(logging.FileHandler("{path}/dac_a2c_train_{curtime}.log".format(path=path, curtime=curtime), "w"))
+        # self.eval_logger.addHandler(logging.FileHandler("{path}/dac_ppo_eval_{curtime}.log".format(path=path, curtime=curtime), "w"))
 
 
     def compute_pi_h(self, prediction, pre_option, is_init_states):
@@ -128,29 +124,7 @@ class DACA2CAgent:
         return advantanges, returns
 
 
-    def random_iter(self, *argv):
-        batch_size = argv[0].size(0)
-        for _ in range(batch_size // self.mini_batch_size):
-            rand_ids = np.random.randint(0, batch_size, self.mini_batch_size)
-            yield (cache[rand_ids, :] for cache in argv)
-
-
-    def permut_iter(self, *argv):
-        batch_size = argv[0].size(0)
-        indices = np.asarray(np.random.permutation(np.arange(batch_size)))
-        batches = indices[:batch_size // self.mini_batch_size * self.mini_batch_size].reshape(-1, self.mini_batch_size)
-        for batch in batches:
-            # yield batch
-            # print("batch: %s" % batch)
-            yield (cache[batch, :] for cache in argv)
-        r = len(indices) % self.mini_batch_size
-        if r:
-            # yield indices[-r:]
-            batch = indices[-r:]
-            # print("batch: %s" % batch)
-            yield (cache[batch, :] for cache in argv)
-
-    def learn(self,
+    def compute_loss(self,
               mdp_type, 
               log_pi,#
               value, # v
@@ -171,7 +145,7 @@ class DACA2CAgent:
 
         # policy_loss = -torch.min(objective, objective_clipped).mean() # + 0.01 * entropy
         # value_loss = 0.5 * (sample_returns - value).pow(2).mean()
-        policy_loss = -(log_pi * advs.detach()).mean() - self.entropy_weight * entropy # detach() ?
+        policy_loss = -(log_pi * advs.detach()).mean()# - self.entropy_weight * entropy # detach() ?
         value_loss = (value - returns.detach()).pow(2).mul(0.5).mean()
 
         loss = policy_loss + value_loss
@@ -188,19 +162,12 @@ class DACA2CAgent:
 
         while self.cur_steps <= self.max_steps:
             rewards_cache = []
-            states_cache = []
             dones_cache = []
-            actions_cache = []
-            options_cache = []
-            prev_options_cache = []
-            init_states_cache = []
             pi_h_cache = []
             log_pi_h_cache = []
             log_pi_l_cache = []
             value_h_cache = []
             value_l_cache = []
-            means_cache = []
-            stds_cache = []
 
             for _ in range(self.num_steps):
                 # print(self.cur_steps)
@@ -213,11 +180,6 @@ class DACA2CAgent:
                 mean = prediction['mean'][self.worker_index, options]
                 std = prediction['std'][self.worker_index, options]
                 actions = torch.distributions.Normal(mean, std).sample()
-
-                pi_l = self.compute_pi_l(options.unsqueeze(-1), actions, prediction["mean"], prediction["std"])
-
-                value_h = (prediction["q_option"] * pi_h).sum(-1).unsqueeze(-1)
-                value_l = prediction["q_option"].gather(1, options.unsqueeze(-1))
 
                 # print("train action shape {}".format(actions.shape))
                 #next_state, reward, done, info = self.envs.step(actions) # done: terminated
@@ -232,20 +194,18 @@ class DACA2CAgent:
                         self.train_logger.info("{cur_steps} {reward}".format(cur_steps=self.cur_steps, reward=cumu_rewd[i]))
                         cumu_rewd[i] = 0
 
+                pi_l = self.compute_pi_l(options.unsqueeze(-1), actions, prediction["mean"], prediction["std"])
+
+                value_h = (prediction["q_option"] * pi_h).sum(-1).unsqueeze(-1)
+                value_l = prediction["q_option"].gather(1, options.unsqueeze(-1))
+
                 rewards_cache.append(tensor(reward).unsqueeze(-1))
-                states_cache.append(tensor(states))
                 dones_cache.append(tensor(done).unsqueeze(-1))
-                actions_cache.append(actions)
-                options_cache.append(options.unsqueeze(-1))
-                prev_options_cache.append(self.prev_options.unsqueeze(-1))
-                init_states_cache.append(self.is_init_states.unsqueeze(-1))
                 pi_h_cache.append(pi_h)
                 log_pi_h_cache.append(pi_h[self.worker_index, options].add(1e-5).log().unsqueeze(-1))
                 log_pi_l_cache.append(pi_l.add(1e-5).log())
                 value_h_cache.append(value_h)
                 value_l_cache.append(value_l)
-                means_cache.append(prediction["mean"])
-                stds_cache.append(prediction["std"])
 
                 self.is_init_states = tensor(done).byte()
                 self.prev_options = options
@@ -262,28 +222,17 @@ class DACA2CAgent:
 
             value_h_cache.append(value_h)
             value_l_cache.append(value_l)
-            means_cache.append(prediction["mean"])
-            stds_cache.append(prediction["std"])
 
             # computer advantange
             adv_h_cache, returns_h_cache = self.compute_adv(value_h_cache, rewards_cache, dones_cache)
             # print("adv_h_cache len %s" % len(adv_h_cache))
             adv_l_cache, returns_l_cache = self.compute_adv(value_l_cache, rewards_cache, dones_cache)
 
-            rewards_cache = cat(rewards_cache, self.num_steps)
-            states_cache = cat(states_cache, self.num_steps)
-            dones_cache = cat(dones_cache, self.num_steps)
-            actions_cache = cat(actions_cache, self.num_steps).detach()
-            options_cache = cat(options_cache, self.num_steps)
-            prev_options_cache = cat(prev_options_cache, self.num_steps)
-            init_states_cache = cat(init_states_cache, self.num_steps)
-            pi_h_cache = cat(pi_h_cache, self.num_steps).detach()
-            log_pi_h_cache = cat(log_pi_h_cache, self.num_steps).detach()
-            log_pi_l_cache = cat(log_pi_l_cache, self.num_steps).detach()
+            pi_h_cache = cat(pi_h_cache, self.num_steps)
+            log_pi_h_cache = cat(log_pi_h_cache, self.num_steps)
+            log_pi_l_cache = cat(log_pi_l_cache, self.num_steps)
             value_h_cache = cat(value_h_cache, self.num_steps)
             value_l_cache = cat(value_l_cache, self.num_steps)
-            means_cache = cat(means_cache, self.num_steps).detach()
-            stds_cache = cat(stds_cache, self.num_steps).detach()
             adv_h_cache = cat(adv_h_cache, self.num_steps)
             returns_h_cache = cat(returns_h_cache, self.num_steps)
             adv_l_cache = cat(adv_l_cache, self.num_steps)
@@ -300,7 +249,7 @@ class DACA2CAgent:
                     return log_pi_l_cache, value_l_cache, returns_l_cache, adv_l_cache
 
             log_pi_cache, value_cache, returns_cache, adv_cache = helper(mdp_types[0])
-            self.learn(
+            loss1 = self.compute_loss(
                 mdp_types[0],
                 log_pi_cache,
                 value_cache,
@@ -310,7 +259,7 @@ class DACA2CAgent:
                 )
 
             log_pi_cache, value_cache, returns_cache, adv_cache = helper(mdp_types[1])
-            self.learn(
+            loss2 = self.compute_loss(
                 mdp_types[1],
                 log_pi_cache,
                 value_cache,
@@ -319,9 +268,15 @@ class DACA2CAgent:
                 pi_h_cache
                 )
 
-            eval_reward = np.mean([self.test_env() for _ in range(10)])
-            print("Evaluation reward at step {cur_steps} is {reward}".format(cur_steps=self.cur_steps, reward=eval_reward))
-            self.eval_logger.info("{cur_steps} {reward}".format(cur_steps=self.cur_steps, reward=eval_reward))
+            self.opt.zero_grad()
+            (loss1 + loss2).backward()
+            # print(loss1 + loss2)
+            torch.nn.utils.clip_grad_norm_(self.dac_net.parameters(), 0.5)
+            self.opt.step()
+
+            # eval_reward = np.mean([self.test_env() for _ in range(10)])
+            # print("Evaluation reward at step {cur_steps} is {reward}".format(cur_steps=self.cur_steps, reward=eval_reward))
+            # self.eval_logger.info("{cur_steps} {reward}".format(cur_steps=self.cur_steps, reward=eval_reward))
 
 
     def test_env(self, vis=False):
@@ -366,7 +321,7 @@ class DACA2CAgent:
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(description='Run PPO on a specific game.')
+    parser = argparse.ArgumentParser(description='Run DAC A2C on a specific game.')
     parser.add_argument('-e', '--env_name', type=str, help='Name of the game', default='HalfCheetah-v2')
     parser.add_argument("-d", "--device", type=str, help="device to run the network on", choices=["cpu", "cuda"], default="cuda")
     parser.add_argument("-s", "--steps", type=int, help="number steps to train agent", default=2e6)
